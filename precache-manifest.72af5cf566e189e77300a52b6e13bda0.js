f5f3067b1d122dcac337e1026116db38adbf0f59
root <root@localhost.localdomain>
2019-07-29 21:16:38 +0300

    Updates

---
+++ precache-manifest.72af5cf566e189e77300a52b6e13bda0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a200549400982cd31b7f921b423d0492",
+    "url": "/5t3ve/costcalculator-ke.git/index.html"
+  },
+  {
+    "revision": "b4fe8648fe00505fd726",
+    "url": "/5t3ve/costcalculator-ke.git/static/css/main.2a22455c.chunk.css"
+  },
+  {
+    "revision": "1c69271d80ccd04d1be0",
+    "url": "/5t3ve/costcalculator-ke.git/static/js/2.3ad88554.chunk.js"
+  },
+  {
+    "revision": "b4fe8648fe00505fd726",
+    "url": "/5t3ve/costcalculator-ke.git/static/js/main.7d748701.chunk.js"
+  },
+  {
+    "revision": "364ddd9cb1c00697337b",
+    "url": "/5t3ve/costcalculator-ke.git/static/js/runtime~main.5603147d.js"
+  }
+]);
